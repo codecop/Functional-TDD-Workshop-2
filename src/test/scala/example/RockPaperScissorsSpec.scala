d42a5e5b72402339690b823594e5ca0e2fcb8028
Gregor Trefs <Gregor.Trefs@gmail.com>
2018-01-25 12:02:19 +0100

    Paper looses to scissors

--- src/test/scala/example/RockPaperScissorsSpec.scala
+++ src/test/scala/example/RockPaperScissorsSpec.scala
@@ -34,10 +34,42 @@ class RockPaperScissorsSpec extends FlatSpec with Matchers {
     Scissors.winsOver(Paper) shouldEqual true
   }
 
+  // General: Two different kind of risks: Implementation and business
+
+  // TC: Paper wins over Rock
+  // Triangulate over winsOver
+  // Shaping types
+
+  // TC: Introduce player as scissors winner
+  // Go wider into scissors over paper
+  // Risk based: Where do I see more uncertainty
+  // More uncertain: Introduce player
+
+  // Go to another entry point
+  // TC: TBD
+  // Not a good idea
+  // Not continuing
+  // Not finishing anything
+  // Large decision tree
+  // Loosing track where you want to go
+  // DFS vs BFS
+
+  // TC: Find a more abstract function: Hand wins over hand
+  // Other view: Can I generalize the concept
+  // Abstract representation for Scissors and Rock
+
+  "Paper" should "not win over scissors" in {
+    Paper.winsOver(Scissors) shouldEqual false
+  }
+
   object Scissors {
     def winsOver(paper: Any) = true
 
   }
 
-  object Paper
+  object Paper {
+    def winsOver(scissors: Any) = false
+
+  }
+
 }
