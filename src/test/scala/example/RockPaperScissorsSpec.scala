f6810eb02139d7a287bd5c6a2cacadf641a96e9c
Gregor Trefs <Gregor.Trefs@gmail.com>
2018-01-25 11:44:49 +0100

    Scissor wins over paper

--- src/test/scala/example/RockPaperScissorsSpec.scala
+++ src/test/scala/example/RockPaperScissorsSpec.scala
@@ -3,9 +3,41 @@ package example
 import org.scalatest._
 
 class RockPaperScissorsSpec extends FlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
+
+  // Two players
+  // One player will Win or it will be a draw --> Sum types?
+  // We fix the agreement in a type
+
+  // Entry points:
+  // 1. Focusing on draw
+  // 2. Focusing on winning
+
+  // Winning -> What do we need to analyse?
+  // S wins over P
+  // R wins over S
+  // P wins over R
+
+  // win method on Scissors?
+  //
+//
+//  "Scissors" should "win over paper" in {
+//    val result = play(Scissors, Paper)
+//    result shouldEqual Win(Scissors)
+//  }
+//
+//  "Scissors" should "win over paper" in {
+//    val result = play(playerOne=Scissors, playerTwo=Paper)
+//    result shouldEqual PlayerOneWins
+//  }
+
+  "Scissors" should "win over paper" in {
+    Scissors.winsOver(Paper) shouldEqual true
   }
 
+  object Scissors {
+    def winsOver(paper: Any) = true
 
+  }
+
+  object Paper
 }
